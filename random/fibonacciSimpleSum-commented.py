92e3e154e208a056450ea241e976f1280faadd49
Harry Henry Gebel <harry@gebel.tech>
2020-12-12 01:36:32 -0500

    Streamline

--- random/fibonacciSimpleSum-commented.py
+++ random/fibonacciSimpleSum-commented.py
@@ -1,20 +1,28 @@
 def fibonacciSimpleSum(number):
     # 0, 1, 2, and 3 are Fibonacci numbers, and all Fibonacci numbers
     # pass because 0 is a Fibonacci number, and x + 0 = x, 4 passes
-    # because 2 + 2 = 4
-    if number <= 5:
+    # because 2 + 2 = 4, etc. 12 is the first number that is not the
+    # sum of 2 Fibonacci numbers
+    if number < 12:
         return True
 
-    # store all the numbers that we haven't matched yet
-    # 0 .. 3 are all Fibonacci numbers
-    numbers = {x for x in range(4)}
+    # Store all the Fibonacci numbers that we have found so far. Seed
+    # set with Fibonacci numbers up to 5. Note: these are all the
+    # Fibonacci numbers up to 5, but they are not the Fibonacci set,
+    # which has 1 twice. For our purposes we don't need the actual
+    # Fibonacci set because we only need a list of numbers we can
+    # check. There is no reason to check a number twice. This allows
+    # us to use the very efficient Python set collection.
+    numbers = {0, 1, 2, 3, 5}
 
-    # the first Fibonacci number we will actually check is 5
-    last_number = 3
-    this_number = 5
+    # the first Fibonacci number we will actually check is 8
+    last_number = 5
+    this_number = 8
 
     while this_number <= number:
-        # see what number we have to add to get the target
+        # Store this number and see what number we have to add to get
+        # the target
+        numbers.add(this_number)
         target = number - this_number
 
         # Check if the target number is a Fibonacci number we've
@@ -24,7 +32,6 @@ def fibonacciSimpleSum(number):
         if target in numbers or target == this_number:
             return True
         else:
-            numbers.add(this_number)
 
         # advance to the next Fibonacci number.
         next_number = last_number + this_number
--- random/fibonacciSimpleSum.py
+++ random/fibonacciSimpleSum.py
@@ -1,19 +1,18 @@
 def fibonacciSimpleSum(number):
-    if number <= 4:
+    if number < 12:
         return True
 
-    numbers = {x for x in range(4)}
+    numbers = {0, 1, 2, 3, 5}
 
-    last_number = 3
-    this_number = 5
+    last_number = 5
+    this_number = 8
 
     while this_number <= number:
+        numbers.add(this_number)
         target = number - this_number
 
-        if target in numbers or target == this_number:
+        if target in numbers:
             return True
-        else:
-            numbers.add(this_number)
 
         next_number = last_number + this_number
         last_number = this_number
